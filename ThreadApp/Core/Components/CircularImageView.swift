d78ec19b28b089b537c612ecd217d6c6c1290eb2
obaro paul <obaroiskool@gmail.com>
2024-05-01 15:34:06 +0100

    Feed cell and search done with some refactor

---
+++ ThreadApp/Core/Components/CircularImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularImageView.swift
+//  ThreadApp
+//
+//  Created by Obaro Paul on 01/05/2024.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularImageView()
+}
---
+++ ThreadApp/Core/Feed/View/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  ThreadApp
+//
+//  Created by Obaro Paul on 01/05/2024.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
